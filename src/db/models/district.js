6a1b63f7bd7434e3b216a085f54c3dd8ffa50f57
zeyadk <zeyad.khaled2401@gmail.com>
2021-08-10 21:09:40 +0200

    feat: add initial models

---
+++ src/db/models/district.js
@@ -0,0 +1,34 @@
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class District extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+            District.hasMany(models["Student"], {
+                foreignKey: "StudentBirthDistrictId"
+            });
+        }
+    };
+    District.init({
+        DistrictId: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        DistrictName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        }
+    }, {
+        sequelize,
+        createdAt: false,
+        modelName: 'District',
+        freezeTableName: true,
+    });
+    return District
+};
