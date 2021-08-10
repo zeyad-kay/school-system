23d29d44566a02f2b5deb6647e103639ef2842a1
zeyadk <zeyad.khaled2401@gmail.com>
2021-08-10 17:42:12 +0200

    feat: initialize migrations

---
+++ src/config/db.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "12345678",
+    "database": "School",
+    "host": "localhost",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
