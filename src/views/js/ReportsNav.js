7db9e37ad7f4cce8805e9ae16c5c2c7238d81de7
zeyad <zeyad_khaled@outlook.com>
2022-02-13 15:45:16 +0200

    fix: remove extra seats dropdown

--- src/views/js/ReportsNav.js
+++ src/views/js/ReportsNav.js
@@ -1,38 +1,36 @@
 // eslint-disable-next-line
 const ReportsNav = (data) => {
-  const seatsList = document.querySelector("#SeatsId");
   const capacitiesList = document.querySelector("#Capacities");
   const agesList = document.querySelector("#Ages");
   const motherList = document.querySelector("#MotherData");
-  const classList = document.querySelector("#ClassList");
+  const classList = document.querySelector("#Report-ClassList");
   let stages = data.stagesData;
   let agesDropDown = "";
   let classListDropDown = "";
-  let seatsDropDown = "";
   let CapacitiesDropDown = "";
   for (let stage of stages) {
-    seatsDropDown += "<li>";
-    seatsDropDown += `
-          <a class="dropdown-item" href="#"> ${stage.StageName} &raquo; </a>
-          <ul class="dropdown-menu dropdown-submenu">
-          `;
+    // seatsDropDown += "<li>";
+    // seatsDropDown += `
+    //       <a class="dropdown-item" href="#"> ${stage.StageName} &raquo; </a>
+    //       <ul class="dropdown-menu dropdown-submenu">
+    //       `;
     classListDropDown += "<li>";
     classListDropDown += `
           <a class="dropdown-item" href="#"> ${stage.StageName} &raquo; </a>
-          <ul class="submenu dropdown-menu">
+          <ul class="dropdown-submenu dropdown-menu">
           `;
     for (let grade of stage.Grades) {
-      seatsDropDown += `
-                      <li>
-                        <a class="dropdown-item btn" href="#" onclick='window.api.send("sendAffairsReportData", ["Seats", [${grade.GradeId}]])'>
-                          ${grade.GradeName}
-                        </a>
-                      </li>
-            `;
+      // seatsDropDown += `
+      //                 <li>
+      //                   <a class="dropdown-item btn" href="#" onclick='window.api.send("sendAffairsReportData", ["Seats", [${grade.GradeId}]])'>
+      //                     ${grade.GradeName}
+      //                   </a>
+      //                 </li>
+      //       `;
       classListDropDown += `
                       <li>
                         <a class="dropdown-item" href="#"> ${grade.GradeName} &raquo; </a>
-                        <ul class="submenu dropdown-menu">
+                        <ul class="dropdown-submenu dropdown-menu">
             `;
       for(let clas of grade.Classes) {
         classListDropDown += `
@@ -45,8 +43,8 @@ const ReportsNav = (data) => {
       }
       classListDropDown += "</ul></li>";
     }
-    seatsDropDown += `</ul>
-          </li>`;
+    // seatsDropDown += `</ul>
+    //       </li>`;
     classListDropDown += `</ul>
           </li>`;
     CapacitiesDropDown += "<li>";
@@ -86,7 +84,6 @@ const ReportsNav = (data) => {
 
   agesList.innerHTML = agesDropDown;
   motherList.innerHTML = agesDropDown.replaceAll("StudentsAges", "motherData");
-  seatsList.innerHTML = seatsDropDown;
   classList.innerHTML = classListDropDown;
   capacitiesList.innerHTML = CapacitiesDropDown;
 };
