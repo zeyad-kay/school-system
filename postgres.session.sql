afc4694b9cffda09b73059964759b448ce33599c
amr1235 <amr.ali99@eng-st.cu.edu.eg>
2022-03-16 23:40:35 +0200

    id reports done

--- postgres.session.sql
+++ postgres.session.sql
@@ -1,8 +1,27 @@
-SELECT "StudentName", "GradeName","ClassName","JobName" FROM "ParentJob"
-JOIN "Student" ON "Student"."StudentResponsibleId" = "ParentJob"."ParentId"
-JOIN "StudentClass" ON "Student"."StudentId" = "StudentClass"."StudentId"
-JOIN "Class" ON "StudentClass"."ClassId" = "Class"."ClassId"
-JOIN "Grade" ON "Class"."GradeId" = "Grade"."GradeId"
-JOIN "Stage" ON "Grade"."StageId" = "Stage"."StageId" 
-JOIN "Job"   ON "Job"."JobId" = "ParentJob"."ParentJobId"
-WHERE "ParentJob"."ParentJobId" = '6'
+SELECT "respMother"."StudentName","StudentNationalId","ResponsibleId","fatherId","MotherId"
+From (
+    SELECT "resp"."StudentName","ResponsibleId","MotherId","resp"."StudentNationalId","resp"."StudentId"
+    from (
+        SELECT "StudentName",
+          "ParentNationalId" AS "ResponsibleId","StudentNationalId","StudentId"
+        FROM "Student"
+          JOIN "Parent" ON "Student"."StudentResponsibleId" = "Parent"."ParentId"
+      ) AS "resp"
+      JOIN (
+        SELECT "StudentName",
+          "ParentNationalId" AS "MotherId"
+        FROM "Student"
+          JOIN "Parent" ON "Student"."StudentMotherId" = "Parent"."ParentId"
+      ) AS "mother" ON "resp"."StudentName" = "mother"."StudentName"
+  ) as "respMother"
+  JOIN (
+    SELECT "StudentName",
+      "ParentNationalId" AS "fatherId"
+    FROM "Student"
+      JOIN "Parent" ON "Student"."StudentFatherId" = "Parent"."ParentId"
+  ) AS "father" ON "respMother"."StudentName" = "father"."StudentName" 
+  JOIN "StudentClass" ON "respMother"."StudentId" = "StudentClass"."StudentId"
+  JOIN "Class" ON "StudentClass"."ClassId" = "Class"."ClassId"
+  JOIN "Grade" ON "Class"."GradeId" = "Grade"."GradeId"
+  JOIN "Stage" ON "Grade"."StageId" = "Stage"."StageId"
+  WHERE "Stage"."StageId" = '1'
